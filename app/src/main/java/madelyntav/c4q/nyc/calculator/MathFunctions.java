3784fe33d28e8a2c432f44ef9f77dbae554bfcdd
MadelynTav <Madelyntav@gmail.com>
2015-05-16 11:58:00 -0400

    Madeys Changes

---
+++ app/src/main/java/madelyntav/c4q/nyc/calculator/MathFunctions.java
@@ -0,0 +1,9 @@
+package madelyntav.c4q.nyc.calculator;
+
+/**
+ * Created by c4q-madelyntavarez on 5/5/15.
+ */
+public class MathFunctions {
+    //TODO implement all mathematical functions
+    //
+}

98b94f5c9e147bd5dc9e6bb30f475f47bc036896
John Gomez <jgomez818@gmail.com>
2015-05-16 11:53:45 -0400

    John's changes on Buttons

--- app/src/main/java/madelyntav/c4q/nyc/calculator/Expressions.java
+++ app/src/main/java/madelyntav/c4q/nyc/calculator/Expressions.java
@@ -204,6 +204,8 @@ public class Expressions {
      */
     public static final BigDecimal PI = new BigDecimal(
             "3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679");
+    public static final BigDecimal e = new BigDecimal(
+            "3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679");
 
     /**
      * The {@link MathContext} to use for calculations.
